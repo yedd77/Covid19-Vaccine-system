c5d52e023e7fa24f5ffddc8594b09655b8582259
trophē <52603129+yedd77@users.noreply.github.com>
2022-06-05 22:48:38 +0800

    V1.2

--- login-engine.php
+++ login-engine.php
@@ -23,20 +23,23 @@ if(isset($_POST['login'])){
         //ref: https://www.php.net/manual/en/function.password-hash.php
         if(password_verify($password , $dbpass)){
             header('location:admin-page.php');
+
+        //invalid password (pass didn't match with pass set in db)
         } else {
             ?>
             <script>
                 alert("Invalid credential, please try again");
-                window.location = "index.html";
+                window.location = "admin-login.html";
             </script>
             <?php
         }
+    
+    //invalid user name (username entered didn't available inside db)
     } else {
-        
         ?>
         <script>
             alert("Invalid Username");
-            window.location = "index.html";
+            window.location = "admin-login.html";
         </script>
         <?php
     }
